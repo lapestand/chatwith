1622677e2c93e4c05819893a52e9029213e6b961
lapestand <izztylmaz1@gmail.com>
2021-06-27 16:16:02 +0300

    error fixed.

--- public_html/application/config/autoload.php
+++ public_html/application/config/autoload.php
@@ -79,7 +79,7 @@ $autoload['libraries'] = array();
 |	$autoload['drivers'] = array('cache' => 'cch');
 |
 */
-$autoload['drivers'] = array("url");
+$autoload['drivers'] = array();
 
 /*
 | -------------------------------------------------------------------
@@ -89,7 +89,7 @@ $autoload['drivers'] = array("url");
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array();
+$autoload['helper'] = array("url");
 
 /*
 | -------------------------------------------------------------------
